2937380433335ba7005e69aa2e768ef827f20836
Yu Gao <gaoyububest@gmail.com>
2020-11-03 20:10:57 +0000

    spinner animation

---
+++ spinner1.js
@@ -0,0 +1,37 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+// ... fill in the rest yourself ...
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\rHOW '); 
+}, 900);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\rARE '); 
+}, 1900);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\rYOU '); 
+}, 2900);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r?   \n'); 
+}, 3900);
